bc37d00a4fc67ba2c059acf6d22058fbb18f57d2
4513ECHO <4513echo@gmail.com>
2021-11-03 22:25:14 +0900

    [update] first commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # zsh-sticky-shift
+
 sticky shift for Zsh
+
+## How to Install
+
+Please use your favorite plugin manager!
+
+For example:
+
+```sh:zinit
+zinit light 4513echo/zsh-stiky-shift
+```
+
+## How to Configure
+
+For example:
+
+```sh
+export STICKY_SHIFT_KEY="@"
+export STIKY_DELAY=0.3
+```
+
+* `STICKY_SHIFT_KEY`
+
+default: `;`
+
+* `STIKY_TABLE`
+
+default: `jis`
+
+**Note:** I don't support US keyboard yet. If you use it, please send me pull request!
+
+* `STIKY_DELAY`
+
+default: 1 (second)
---
+++ sticky-shift.plugin.zsh
@@ -0,0 +1,53 @@
+STICKY_SHIFT_KEY="${STICKY_SHIFT_KEY:-;}"
+STICKY_TABLE="${STICKY_TABLE:-jis}"
+STICKY_DELAY="${STICKY_DELAY:-1}"
+
+typeset -A jis_table us_table
+jis_table=(
+  '_1' '!' '_2' '"' '_3' '#' '_4' '$' '_5' '%' '_6' '&' '_7' \'
+  '_8' '(' '_9' ')' '_-' '=' '_^' '~' '_¥' '|' '_@' '`' '_[' '{'
+  '_;' '+' '_:' '*' '_]' '}' '_,' '<' '_.' '>' '_/' '?'
+)
+# TODO: support US keyboard
+us_table=(
+
+)
+
+function sticky-shift () {
+  local char result old_RBUFFER
+  typeset -A sticky_table special_table
+
+  if [[ "$STICKY_TABLE" = "jis" ]]; then
+    set -A sticky_table ${(kv)jis_table}
+  else
+    set -A sticky_table ${(kv)us_table}
+  fi
+  special_table=(
+    "_ " $STICKY_SHIFT_KEY "_" $STICKY_SHIFT_KEY
+  )
+
+  old_RBUFFER="$RBUFFER"
+  RBUFFER=";$RBUFFER"
+  zle reset-prompt
+
+  while :;do
+    read -k 1 -t $STICKY_DELAY char
+    if [[ "$char" =~ [a-z] ]]; then
+      result="$(tr '[:lower:]' '[:upper:]' < <(echo "$char"))"
+      break
+    elif [[ -n "${sticky_table[(i)_$char]}" ]]; then
+      result="${sticky_table[_$char]}"
+      break
+    elif [[ -n "${special_table[(i)_$char]}" ]]; then
+      result="${special_table[_$char]}"
+      break
+    fi
+  done
+
+  RBUFFER="$old_RBUFFER"
+  LBUFFER="$LBUFFER$result"
+}
+
+zle -N sticky-shift
+bindkey "$STICKY_SHIFT_KEY" sticky-shift
+
